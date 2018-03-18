bc6e116acb030d90f239cfb10404f91f105f3369
Camilo <qnelo@hotmail.com>
2018-03-18 17:50:56 -0300

    implement sender and getter message

--- README.md
+++ README.md
@@ -3,4 +3,8 @@ serverless create --template aws-nodejs --path serverless-sns-example
 cd serverless-sns-example
 git init
 yarn init
+yarn add aws-sdk
+yarn add serverless-offline --dev
+yarn add serverless-offline-sns --dev
+yarn add serverless-plugin-optimize --dev
 ```
--- handler.js
+++ handler.js
@@ -1,13 +1,65 @@
 'use strict';
 
-module.exports.message = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+const sendMessage = (event, context, callback) => {
+
+  const AWS = require('aws-sdk');
+  const config = require('./config')(process.env.IS_OFFLINE);
+
+  const snsConfig = process.env.IS_OFFLINE
+    ? {
+      endpoint: 'http://127.0.0.1:4002',
+      region: config.region,
+    }
+    : {};
+
+  const message = {
+    body: JSON.stringify({ message: 'Ultra Test Message' })
   };
 
-  callback(null, response);
+  const snsPublishConfig = {
+    Message: message,
+    MessageStructure: 'json',
+    TopicArn: `arn:aws:sns:${config.region}:${config.awsAccountId}:sns-example-topic`,
+  };
+
+  const sns = new AWS.SNS(snsConfig);
+  const snsPublish = sns.publish(snsPublishConfig).promise();
+
+  snsPublish.then(
+    () => {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'message sent' })
+      }
+      callback(null, response);
+    }
+  ).catch(
+    err => {
+      console.error('ERROR: ', err);
+    }
+  );
+  // sns.publish({
+  //   Message: 'Ultra Test Message',
+  //   MessageStructure: 'json',
+  //   TopicArn: 'arn:aws:sns:us-east-1:123456789012:sns-example-topic',
+  // }, () => {
+  //   const response = {
+  //     statusCode: 200,
+  //     body: JSON.stringify({
+  //       message: 'message sent'
+  //     }),
+  //   };
+
+  //   callback(null, response);
+  // });
 };
+
+const getMessage = (event, context, callback) => {
+  console.info('incoming message: ', event.Records[0].Sns.Message);
+  callback(null, { response: 'message received' });
+};
+
+module.exports = {
+  sendMessage,
+  getMessage
+}
